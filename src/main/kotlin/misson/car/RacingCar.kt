6c49d19e9012063e046d50c40110335b29197dec
Aimbe <qkr2435@gmail.com>
2024-11-17 17:33:08 +0900

    feat: RacingCar객체의 이름 프로퍼티 추가

--- src/main/kotlin/misson/car/RacingCar.kt
+++ src/main/kotlin/misson/car/RacingCar.kt
@@ -1,8 +1,9 @@
 package misson.car
 
-class RacingCar {
-    var position: Int = 0
-        private set
+class RacingCar(private val name: String = "붕붕이", private var position: Int = 0) {
+    init {
+        require(name.length in 1..5) { "이름은 1자 이상 5자 이하여야 합니다." }
+    }
 
     fun move(randomNumberGenerator: () -> Int) {
         if (randomNumberGenerator() >= 4) {
