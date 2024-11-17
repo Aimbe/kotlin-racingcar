6e9d6036da9cc6c302e15b0301b651d0485d4956
Aimbe <qkr2435@gmail.com>
2024-11-17 18:15:58 +0900

    feat: winner 객체 책임 분리

--- src/main/kotlin/misson/car/RacingCars.kt
+++ src/main/kotlin/misson/car/RacingCars.kt
@@ -12,4 +12,11 @@ class RacingCars(private val cars: List<RacingCar>) {
     fun getPositionsWithName(): List<String> {
         return cars.map { it.representPostitionWithName() }
     }
+
+    // 우승자를 찾는 객체의 책임
+    fun findWinner(): Winner {
+        val maxPosition = cars.maxOf { it.position }
+        val winners = cars.filter { it.position == maxPosition }
+        return Winner(winners)
+    }
 }
